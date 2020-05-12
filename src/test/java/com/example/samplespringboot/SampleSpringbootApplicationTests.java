2971b1302292550dcf6cb078387c2e6f5ca32b45
murata <murata@infoscience.co.jp>
2020-05-12 13:38:04 +0900

    簡単なAPIを作成

---
+++ src/test/java/com/example/samplespringboot/SampleSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
